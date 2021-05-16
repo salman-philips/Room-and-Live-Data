140f97c967cf52f91b28c3f283530bf0f1544e1a
salman-philips <mohamedsalman.siddiq@philips.com>
2021-05-16 12:33:23 +0530

    abR_02:Added the a custom  launcher icon

--- app/src/main/java/com/roomandlivedataroom1/MainActivity.java
+++ app/src/main/java/com/roomandlivedataroom1/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// you can fo to image assets and check for adaptive and legacy options
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
